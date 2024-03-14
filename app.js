df178285bbe0f273da04c8850bbad240080fc0dc
Ahsan Ahmed <ahsanahmed9104@gmail.com>
2024-03-14 23:29:15 +0530

    Morgan introduced

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express();
 
 require("./models/dbconfig").dbconnection();
 
+// routes
+const userRouter = require("./routes/userRoute");
+//logger
+const logger = (require("morgan"));
+app.use(logger("tiny"));
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/api/user",userRouter);
+// server
 app.listen(process.env.PORT, () => {
     console.log(`The Server is running on port ${process.env.PORT}`);
 });
